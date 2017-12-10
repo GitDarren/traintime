bd273b09295ccf78d4699fb61959c437cc54c377
GitDarren <darren321@gmail.com>
2017-12-10 13:25:54 -0600

    Fixed everything regarding Moment.js but the first minutes Away result is giving a NaN.  No idea why.

--- scripts/trainapp.js
+++ scripts/trainapp.js
@@ -16,22 +16,50 @@ var database = firebase.database();
 
 var name = "";
 var dest = "";
-var startDate = moment().format("MM-DD-YYYY");
-var monthlyRate = 0;
-var monthsWorked;
-var totalBilled;
+var timeFrequency = 0;
+var firstTime = 0;
+var timeRemainder = 0;
 
-database.ref().on("child_added", function (snapshot) {
+
+
+database.ref().orderByChild("inputDest").on("child_added", function (snapshot) {
     console.log('here')
+
+    var firstTimeCoverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    console.log("This is first time converted = " + firstTimeCoverted);
+
+    var currentTime = moment();
+    console.log(currentTime);
+
+    var diffTime = moment().diff(moment(firstTimeCoverted), "minutes");
+    console.log("Difference in Time: " + diffTime);
+
+    timeRemainder = diffTime % timeFrequency;
+    console.log(timeRemainder);
+
+    var minutesAway = timeFrequency - timeRemainder;
+    console.log("Minutes until the goddamn train: " + minutesAway);
+
+    var nextArrival = moment().add(minutesAway, "minutes");
+    console.log("The fucking train will arrive at " + moment(nextArrival).format("hh:mm"));
+
+    timeFrequency = snapshot.val().inputFreq;
+    console.log("This is time frequency " + timeFrequency);
+
+    firstTime = snapshot.val().inputFirstTrain;
+    console.log("This is first time " + firstTime);
+    
+
+
     $('#dostuff').append(
         `<tr class="active">
         <td id="table-name">${snapshot.val().inputName}</td>
         <td id="table-dest">${snapshot.val().inputDest}</td>
         <td id="table-freq">${snapshot.val().inputFreq}</td>
-        <td id="table-nextarr"></td>
-        <td id="table-minutes"></td>
-    </tr>
-    `)
+        <td id="table-nextarr">${moment(nextArrival).format("hh:mm")}</td>
+        <td id="table-minutes">${minutesAway}</td>
+    </tr>`
+    )
 
 });
 
@@ -41,7 +69,7 @@ database.ref().on("child_added", function (snapshot) {
 
         var inputName = $("#train-name").val().trim();
         var inputDest = $("#destination").val().trim();
-        var inputFirstTrain = parseInt($("#first-train").val().trim());
+        var inputFirstTrain = $("#first-train").val().trim();
         var inputFreq = $("#frequency").val().trim();
 
         console.log(inputName);
@@ -56,7 +84,6 @@ database.ref().on("child_added", function (snapshot) {
             inputFreq: inputFreq
         })
         $('input').val('');
-
     });
 
 
